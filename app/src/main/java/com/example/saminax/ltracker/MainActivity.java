2200feee987a769ba3966965e5d81397ef7d6632
Skyknot <samina.cse09@gmail.com>
2016-11-14 16:09:21 +0600

    FusedLocationApi.getLastLocation() succeed with FINE_LOCATION permission

--- app/src/main/java/com/example/saminax/ltracker/MainActivity.java
+++ app/src/main/java/com/example/saminax/ltracker/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.saminax.ltracker;
 
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -11,10 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
@@ -22,21 +18,26 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     public GoogleApiClient mGoogleApiClient;
 
     Location mLastLocation;
-    public TextView mLatitudeText, mLongitudeText;
-
-    //public TextView T;
-
+    TextView mLatitudeText, mLongitudeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initializeFields();
+        connectGoogleAPI();
+
+    }
+
+    void initializeFields(){
         mLatitudeText = (TextView) findViewById(R.id.mLatitudeText);
         mLongitudeText = (TextView) findViewById(R.id.mLongitudeText);
         mLatitudeText.setText("Latitude");
         mLongitudeText.setText("Longitude");
+    }
 
+    void connectGoogleAPI(){
         // Create an instance of GoogleAPIClient.
         if (mGoogleApiClient == null)
             mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -57,14 +58,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onStop();
     }
 
-
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        //System.out.println("--------onConnected");
-        Log.e("Samina","------onConnected Permission value:"+ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION));
+        Log.e("Samina","------onConnected Permission value:"+ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION));
         Log.e("Samina","------onConnected PackageManagerPermission value:"+PackageManager.PERMISSION_GRANTED);
-        if (
-                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -72,12 +70,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            Log.e("Samina","could not connect");
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 200);
-
+            Log.e("Samina","Could not connect GoogleAPI");
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 200);
         }
         else{
-            Log.e("Samina","connected successfully");
+            Log.e("Samina","GoogleAPI Connected successfully");
 
         }
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
@@ -96,6 +93,5 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Log.e("Samina","-------onConnectionFailed");
-
     }
 }
