abd7c6b109dc0c1dc09b84fe337316b2c611a4ef
Skyknot <samina.cse09@gmail.com>
2016-12-16 20:30:43 +0600

    Copyright

--- app/src/main/java/com/example/saminax/ltracker/MainActivity.java
+++ app/src/main/java/com/example/saminax/ltracker/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.saminax.ltracker;
 //AIzaSyAkrO0LPwQDPFdp6BwQmuIMKIcU0xcdt6c//
 
-import android.app.FragmentTransaction;
-import android.content.DialogInterface;
+
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.net.wifi.p2p.WifiP2pManager;
+
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,29 +16,33 @@ import android.widget.Button;
 import android.widget.TextView;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MainActivity extends AppCompatActivity implements  OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     public GoogleApiClient mGoogleApiClient;
 
     Location mCurrentLocation;
     TextView mLatitudeText, mLongitudeText;
-    GoogleMap mMap;
     MapFragment mMapFragment;
-    double latitude,longitude;
-    Marker mMarker;
+    double latitude, longitude;
+    LocationRequest mLocationRequest;
+    PendingResult<LocationSettingsResult> result;
+    LocationSettingsRequest.Builder builder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,32 +54,91 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
 
         mMapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.mMapFragment);
-        //mMapFragment.getMapAsync(this);
+
         Button buttonRefresh = (Button) findViewById(R.id.buttonRefresh);
         buttonRefresh.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                onConnected(null);
+                mGoogleApiClient.disconnect();
+                mGoogleApiClient.connect();
             }
         });
+
+       /* builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+        result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,
+                        builder.build());*/
 
 
     }
 
-    void initializeFields(){
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.e("Samina", "------onConnected Permission value:" + ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION));
+        Log.e("Samina", "------onConnected PackageManagerPermission value:" + PackageManager.PERMISSION_GRANTED);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.e("Samina", "Could not connect GoogleAPI");
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        } else {
+            Log.e("Samina", "GoogleAPI Connected successfully");
+
+        }
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mCurrentLocation != null) {
+            mLatitudeText.setText(String.valueOf(mCurrentLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mCurrentLocation.getLongitude()));
+
+            latitude = mCurrentLocation.getLatitude();
+            longitude = mCurrentLocation.getLongitude();
+
+            mMapFragment.getMapAsync(this);
+
+        }
+
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e("Samina", "--------onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("Samina", "-------onConnectionFailed");
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        Log.e("Samina", "-------onMapReady");
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(latitude, longitude))
+                .title("Marker"));
+
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(latitude, longitude)).zoom(14.0f).build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        map.moveCamera(cameraUpdate);
+    }
+
+    protected void createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    void initializeFields() {
         mLatitudeText = (TextView) findViewById(R.id.mLatitudeText);
         mLongitudeText = (TextView) findViewById(R.id.mLongitudeText);
         mLatitudeText.setText("Latitude");
         mLongitudeText.setText("Longitude");
 
-
-
-
-
-        //mMapFragment =(MapFragment) findFragmentById(R.id.mMapFragment);
     }
 
-    void connectGoogleAPI(){
-        // Create an instance of GoogleAPIClient.
+    void connectGoogleAPI() {
+
         if (mGoogleApiClient == null)
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
@@ -97,66 +158,4 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
         super.onStop();
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        Log.e("Samina","------onConnected Permission value:"+ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION));
-        Log.e("Samina","------onConnected PackageManagerPermission value:"+PackageManager.PERMISSION_GRANTED);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            Log.e("Samina","Could not connect GoogleAPI");
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 200);
-        }
-        else{
-            Log.e("Samina","GoogleAPI Connected successfully");
-
-        }
-        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
-        if (mCurrentLocation != null) {
-            mLatitudeText.setText(String.valueOf(mCurrentLocation.getLatitude()));
-            mLongitudeText.setText(String.valueOf(mCurrentLocation.getLongitude()));
-
-            latitude=mCurrentLocation.getLatitude();
-            longitude=mCurrentLocation.getLongitude();
-
-            mMapFragment.getMapAsync(this);
-            //mMapFragment.getMapAsync(this);
-
-
-        }
-    }
-
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.e("Samina","--------onConnectionSuspended");
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.e("Samina","-------onConnectionFailed");
-    }
-
-    /*@Override
-    public void onMapReady(GoogleMap googleMap) {
-        Log.e("Samina","-------onMapReady");
-    }*/
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-        Log.e("Samina","-------onMapReady");
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(latitude,longitude))
-                .title("Marker"));
-
-        CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(latitude,longitude)).zoom(14.0f).build();
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
-        map.moveCamera(cameraUpdate);
-    }
 }
